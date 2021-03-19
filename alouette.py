5d028ec54659d8c709e5a114bdba2bb98e4eae11
Cole DeMan <cole@weusthem.com>
2021-03-19 10:40:03 -0300

    fixed some issues with labels I had noticed

--- alouette.py
+++ alouette.py
@@ -496,20 +496,23 @@ def build_filtering():
                         ),
                         html.Div(
                             [
-                                html.P(
-                                    id="groundstations-text",
-                                    className="control_label",
-                                ),
                                 html.Div([
-                                    html.Label(
-                                        dcc.Dropdown(
-                                            id="ground_station_list",
-                                            options=[],
-                                            placeholder=_("Sélectionner | Select"),
-                                            multi=True,
-                                            value=station_values,
-                                            className="dcc_control"
-                                        ),
+                                    html.Span(
+                                        [
+                                            html.Label(
+                                                id="groundstations-text",
+                                                htmlFor="ground_station_list",
+                                                className="control_label",
+                                            ),
+                                            dcc.Dropdown(
+                                                id="ground_station_list",
+                                                options=[],
+                                                placeholder=_("Sélectionner | Select"),
+                                                multi=True,
+                                                value=station_values,
+                                                className="dcc_control"
+                                            ),
+                                        ],
                                         className="drop_down",
                                     ),
                                     html.Span(children=html.P(id="ground_station_selection"),className="wb-inv")]),
@@ -606,26 +609,31 @@ def build_stats():
                     [
                         html.Div(
                             [
-                                html.P(
-                                    id="x-axis-selection-text",
-                                    className="control_label",
-                                ),
-                                html.Label(
-                                    dcc.Dropdown(
-                                        id="x_axis_selection_1",
-                                        options=x_axis_options,
-                                        multi=False,
-                                        value='timestamp',
-                                        className="dcc_control"
-                                    ),
+                                html.Span(
+                                    [
+                                        html.Label(
+                                            htmlFor='x_axis_selection_1',
+                                            id="x-axis-selection-text",
+                                            className="control_label",
+                                        ),
+                                        dcc.Dropdown(
+                                            id="x_axis_selection_1",
+                                            options=x_axis_options,
+                                            multi=False,
+                                            value='timestamp',
+                                            className="dcc_control"
+                                        ),
+                                    ],
                                     className="drop_down",
                                     role="listbox"
                                 ),
-                                html.P(
-                                    id="y-axis-selection-text",
-                                    className="control_label",
-                                ),
-                                html.Label(
+                                html.Span(
+                                    [
+                                    html.Label(
+                                        htmlFor='y_axis_selection_1',
+                                        id="y-axis-selection-text",
+                                        className="control_label",
+                                    ),
                                     dcc.Dropdown(
                                         id="y_axis_selection_1",
                                         options=y_axis_options,
@@ -633,6 +641,7 @@ def build_stats():
                                         value='max_depth',
                                         className="dcc_control",
                                     ),
+                                    ],
                                     className="drop_down",
                                     role="listbox"
                                 ),
@@ -671,36 +680,42 @@ def build_stats():
                     [
                         html.Div(
                             [
-                                html.P(
-                                    id="stat-selection-text",
-                                    className="control_label",
-                                ),
-                                html.Label(
-                                    dcc.Dropdown(
-                                        id="stat_selection",
-                                        options=[
-                                            {'label': 'Mean', 'value': 'mean'},
-                                            {'label': 'Median', 'value': 'median'}
-                                        ],
-                                        multi=False,
-                                        value='mean',
-                                        className="dcc_control",
-                                    ),
+                                html.Span(
+                                    [
+                                        html.Label(
+                                            htmlFor="stat_selection",
+                                            id="stat-selection-text",
+                                            className="control_label",
+                                        ),
+                                        dcc.Dropdown(
+                                            id="stat_selection",
+                                            options=[
+                                                {'label': 'Mean', 'value': 'mean'},
+                                                {'label': 'Median', 'value': 'median'}
+                                            ],
+                                            multi=False,
+                                            value='mean',
+                                            className="dcc_control",
+                                        ),
+                                    ],
                                     className="drop_down",
                                     role="listbox"
                                 ),
-                                html.P(
-                                    id="stat-y-axis-text",
-                                    className="control_label",
-                                ),
-                                html.Label(
-                                    dcc.Dropdown(
-                                        id="y_axis_selection_2",
-                                        options=y_axis_options,
-                                        multi=False,
-                                        value='max_depth',
-                                        className="dcc_control",
-                                    ),
+                                html.Span(
+                                    [
+                                        html.Label(
+                                            htmlFor="y_axis_selection_2",
+                                            id="stat-y-axis-text",
+                                            className="control_label",
+                                        ),
+                                        dcc.Dropdown(
+                                            id="y_axis_selection_2",
+                                            options=y_axis_options,
+                                            multi=False,
+                                            value='max_depth',
+                                            className="dcc_control",
+                                        ),
+                                    ],
                                     className="drop_down",
                                     role="listbox"
                                 ),
