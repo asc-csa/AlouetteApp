d24de58f6eeebc8a72d7712889163fba79cf1b67
dreneuw <davrene11@gmail.com>
2021-04-27 15:54:15 -0400

    Full implementation of text tables
    Full implementation of text tables for all 4 graphs, including translations

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os.path
 
 class Config:
     LANGUAGES = {'en': 'English', 'fr': 'French'}
-    DEFAULT_LANGUAGE = 'fr'
+    DEFAULT_LANGUAGE = 'en'
     APP_PREFIX = '/alouette/'
     DATA_PATH = os.path.dirname(os.path.abspath(__file__)) + r'/../../data'
     EN_LINK = '/alouette' #url for the english version
